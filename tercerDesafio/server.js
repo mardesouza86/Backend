4edcbab75b0fe6ad123b062fbaff2503eb977d40
mardesouza86 <desouzamartin@gmail.com>
2022-07-01 02:10:52 -0300

    Tercer  Desafio Entregable Corregido

--- tercerDesafio/server.js
+++ tercerDesafio/server.js
@@ -1,11 +1,46 @@
-const http = require('http');
+const express = require('express');
+const fs = require('fs')
 
-const server = http.createServer( (request, response) => {
-    response.end("Quita el contenido viejo - Agrega el contenido nuevo!")
-});
 
-const PORT = 3000;
+const app = express()
 
-server.listen(PORT, () => {
-    console.log(`>>>> Este Servidor esta escuchando http://localhost:${PORT}`)
-});
+const PORT = process.env.PORT || 8080
+
+let productos = []
+
+let productoX = {}
+
+// Defino la funcion de Obtener Productos con async / Retorno el array parseado
+
+async function getProductos (){
+    try {
+        const arr = await fs.promises.readFile('productos.txt', 'utf-8');
+        const arrParsed = JSON.parse(arr);
+        return arrParsed;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Defino como obtengo todos los productos y un X producto en particular
+
+(async function(){
+    productos = await getProductos()
+    productoX = productos[Math.floor(Math.random()*productos.length)]
+})();
+
+// Pongo al server en escucha continuamente
+
+const server = app.listen(PORT,() => {
+    console.log(`Escuchando puerto ${server.address().port}`);
+})
+
+// Defino rutas del server
+
+app.get('/productos',(req,res) => {
+    res.send(productos)
+})
+
+app.get('/productoX',(req,res) => {
+    res.send(productoX)
+})
